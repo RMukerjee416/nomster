5056e28cccef532cf1da8203192c83e72278ede0
Rajkumar <raj_muk@hotmail.com>
2019-05-05 04:18:14 +0000

    Updated bundler

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,8 @@ end
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
