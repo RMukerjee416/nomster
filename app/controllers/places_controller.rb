05939a09821a99cac9a38891d18ae2a14b9d857b
Rajkumar <raj_muk@hotmail.com>
2019-05-06 03:27:08 +0000

    Restricted foreign users from editing places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 	
 	def index
 		@places = Place.all.paginate(page: params[:page], per_page: 5).order("created_at DESC")
@@ -20,10 +20,17 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render plain: 'Not Allowed! Please sign in!'
+		end
 	end
 
 	def update
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render plain: 'Not Allowed! Please sign in!'
+		end
+
 		@place.update_attributes(place_params)
 		redirect_to root_path
 	end
