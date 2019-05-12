551e755013e864ac9abb68634bfc69071f3ceead
Rajkumar <raj_muk@hotmail.com>
2019-05-12 18:20:20 +0000

    Added upload button for photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,10 @@
 class Photo < ApplicationRecord
+	mount_uploader :picture, PictureUploader
+	validates :picture, presence: true
+
 	belongs_to :user
 	belongs_to :place
 
 	validates :caption, presence: true, length: { maximum: 140, minimum: 5 }
-	validates :picture, presence: true
+	
 end
