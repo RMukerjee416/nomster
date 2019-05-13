007db79390492d3ca53507256004abc1d79d7fee
Rajkumar <raj_muk@hotmail.com>
2019-05-13 16:54:28 +0000

    Modified config file to allow access to Amazon S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,7 @@
 CarrierWave.configure do |config|
+
+config.fog_public = false 
+
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
