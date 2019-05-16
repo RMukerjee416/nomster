07ed28b47e71361eaaa13e9f831872c02aff8d8e
Rajkumar <raj_muk@hotmail.com>
2019-05-16 00:42:35 +0000

    Checked automated emails and returned back to test mode

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ApplicationRecord
 	validates :message, presence: true, length: { maximum: 140, minimum: 5 }
  	validates :rating, presence: true
 
+ 	after_create :send_comment_email
+
 	RATINGS = {
 		'one star': '1_star',
 		'two stars': '2_stars',
@@ -15,4 +17,9 @@ class Comment < ApplicationRecord
 	def humanized_rating
 		RATINGS.invert[self.rating]
 	end
+
+	def send_comment_email
+		NotificationMailer.comment_added(self).deliver_now
+	end 
+
 end
